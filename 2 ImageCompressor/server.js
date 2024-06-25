c179a1ae90ff348e257a9af0382ba42ce4c293fa
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-06-25 18:29:47 +0530

    Add Image Compressors

---
+++ 2 ImageCompressor/server.js
@@ -0,0 +1,37 @@
+const sharp = require('sharp');
+const fs = require('fs-extra');
+const path = require('path');
+
+// Specify the directories
+const inputDirectory = ""; // Replace with the path to your input folder
+const outputDirectory = ""; // Replace with the path to your output folder
+
+// Create the output directory if it doesn't exist
+fs.ensureDirSync(outputDirectory);
+
+// Function to compress images
+fs.readdir(inputDirectory, (err, files) => {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+
+    files.forEach(file => {
+        const inputFilePath = path.join(inputDirectory, file);
+        const outputFilePath = path.join(outputDirectory, file);
+
+        // Check if the file is an image
+        const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.webp'];
+        if (imageExtensions.includes(path.extname(file).toLowerCase())) {
+            sharp(inputFilePath)
+                .resize({ width: 1500 }) // Resize the image to a width of 800px
+                .toFormat('jpeg', { quality: 100 }) // Compress the image with 80% quality
+                .toFile(outputFilePath, (err, info) => {
+                    if (err) {
+                        console.log(`Error compressing file ${file}:`, err);
+                    } else {
+                        console.log(`Compressed: ${file}`);
+                    }
+                });
+        }
+    });
+});
